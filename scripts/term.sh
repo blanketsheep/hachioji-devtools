acd5865b3428e77ef3e61b0ccd434fd4568889cf
Shuma Yoshioka <s64.stdio@gmail.com>
2014-04-02 12:15:22 +0900

    Update

--- scripts/term.sh
+++ scripts/term.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ~/git/hachioji
 cd ~/git/hachioji
-git clone "https://github.com/blanketsheep/hachioji.git" .
-roxterm --tab -e "subl --project ~/git/hachioji/"
+git clone "https://github.com/blanketsheep/hachioji.git" ~/git/hachioji
+roxterm --tab -e "subl --new-window --wait ~/git/hachioji"
 roxterm --tab -e "gitg"
