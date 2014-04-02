c508af22e40f3f12b08350ab8e782381e4c4dc1c
Shuma Yoshioka <s64.stdio@gmail.com>
2014-04-02 13:23:28 +0900

    Update default.rb
    日本語対応を分割してみる

--- chef-repo/site-cookbooks/hachioji-devtools/recipes/default.rb
+++ chef-repo/site-cookbooks/hachioji-devtools/recipes/default.rb
@@ -1,6 +1,8 @@
 execute "ubuntu-japanese-team" do
   command 'wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-key add -; wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add -; sudo wget https://www.ubuntulinux.jp/sources.list.d/saucy.list -O /etc/apt/sources.list.d/ubuntu-ja.list; sudo apt-get update'
 end
+package 'language-pack-ja'
+package 'language-pack-gnome-ja'
 package 'ubuntu-defaults-ja'
 
 package 'python-software-properties'
