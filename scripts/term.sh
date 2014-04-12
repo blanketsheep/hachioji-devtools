42c5640e70a00dd1fe7b720832daf783a6d83e0a
Shuma Yoshioka <s64.stdio@gmail.com>
2014-04-12 09:37:59 +0900

    Update term.sh

--- scripts/term.sh
+++ scripts/term.sh
@@ -2,5 +2,7 @@
 mkdir -p ~/git/hachioji
 cd ~/git/hachioji
 git clone "https://github.com/blanketsheep/hachioji.git" ~/git/hachioji
-roxterm --tab -e "subl --new-window --wait ~/git/hachioji"
-roxterm --tab -e "gitg"
+roxterm --tab -e "subl --new-window --wait ~/git/hachioji" -T "Sublime Text"
+roxterm --tab -e "gitg" -T "gitg"
+roxterm --tab -e "jekyll serve --watch --port 4000 --drafts --future" -T "Jekyll"
+roxterm --tab
