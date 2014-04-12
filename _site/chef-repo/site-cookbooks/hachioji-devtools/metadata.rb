442d72621a828766582fc6d257f0cc7a48ca477f
Shuma Yoshioka <s64.stdio@gmail.com>
2014-04-12 09:46:27 +0900

    fix delete _site/

--- _site/chef-repo/site-cookbooks/hachioji-devtools/metadata.rb
+++
@@ -1,2 +0,0 @@
-name             'hachioji-devtools'
-version          '0.1.0'
--- _site/start.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-vagrant up
-start "" config.xlaunch
-vagrant ssh -c "/vagrant/scripts/start_guest.sh"
-pause
-exit
